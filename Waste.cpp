9d7b204683dcb6700ff57d9ede6108a47a27f0e5
schoolprojectacc <95316564+schoolprojectacc@users.noreply.github.com>
2021-12-15 11:44:22 +0000

    Update Waste.cpp

--- Waste.cpp
+++ Waste.cpp
@@ -1,10 +1,16 @@
 #include "Waste.h"
 
-Waste::Waste()
+Waste::Waste(float wasteX, float wasteY)
 
 {
 
-
+	w_Sprite = Sprite(TileManager::GetTexture("graphics/oilspill.png"));
+	
+	w_Position.x = wasteX;
+	w_Position.y = wasteY;
+	w_Sprite.setScale(0.1, 0.1);
+	w_Sprite.setOrigin(25, 25);
+	w_Sprite.setPosition(w_Position);
 
 }
 
@@ -12,6 +18,13 @@ void Waste::status()
 
 {
 
+	if (w_health <= 0)
 
+	{
+
+		wasteLeft--;
+
+
+	}
 
 }
