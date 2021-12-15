2a75f6a31ec4511c66f7d5a7a9025d2ff8b420bb
schoolprojectacc <95316564+schoolprojectacc@users.noreply.github.com>
2021-12-15 11:44:40 +0000

    Update Waste.h

--- Waste.h
+++ Waste.h
@@ -2,18 +2,28 @@
 #ifndef WASTE_H
 #define WASTE_H
 
+#include "TileManager.h"
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
 class Waste
 
 {
 
 public:
-	Waste();
+	Waste(float wasteX, float wasteY);
 	void status();
 
 private:
 	int wasteLeft = 0;
+	int w_health = 0;
 	float wasteX = 0;
 	float wasteY = 0;
+	
+	Sprite w_Sprite;
+	Texture w_Texture;
+	Vector2f w_Position;
 };
 
 #endif
